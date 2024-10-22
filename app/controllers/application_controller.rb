9a846d3889fc90ab187ae80876d776592ea415e2
Eva Touboulic <etouboulic@gmail.com>
2024-10-22 12:01:27 +0200

    config devise+devise fr / table users / install pgsql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action do
+    I18n.locale = :fr
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :avatar])
+  end
 end
