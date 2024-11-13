ace309a83719a4346d592fa1eb8bb5e7195adb55
Eva Touboulic <etouboulic@gmail.com>
2024-11-13 11:42:34 +0100

    Modify log condition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     @organization = Organization.find_by(creator: current_user)
-    @member_organization = Organization.find(current_user.user_orgs.first.organization_id)
+    @member_organization = Organization.find_by(creator: current_user) || Organization.find(current_user.user_orgs.first.organization_id)
   end
 end
