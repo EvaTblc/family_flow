b913a827a8ff7df2221d21206da6ae6be37b6e68
Eva Touboulic <etouboulic@gmail.com>
2024-12-02 17:22:18 +0100

    turbo stream for last message (doesnt work..)

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   after_create_commit :broadcast_message
+  after_commit -> { broadcast_update_to "last_message", partial: "organizations/last_message",
+    locals: { message: self }, target: "last_message" }
 
   private
 
@@ -12,5 +14,4 @@ class Message < ApplicationRecord
                         target: "messages",
                         locals: { message: self, user: user }
   end
-
 end
